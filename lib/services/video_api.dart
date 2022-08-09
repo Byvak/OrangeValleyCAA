8d57053e0955a09fd3a02b59aa08cba1cd74216f
Burnny Kibonge <kibonge.burnny@gmail.com>
2022-08-09 13:56:04 +0100

    Created video_api.dart
    This file is fecthing data

---
+++ lib/services/video_api.dart
@@ -0,0 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'package:orange_valley_caa/models/video.dart';
+import 'package:orange_valley_caa/utils/helper.dart';
+
+Future<List<Video>> getVideoFromApi({VideoSort filter = VideoSort.id}) async {
+  final url = Uri.http(
+      "orangevalleycaa.org", 'api/videos/order/${filter.filterName()}');
+  var response = await http.get(url);
+  if (response.statusCode == 200) {
+    var json = response.body;
+    return videoFromJson(json);
+  } else {
+    return [];
+  }
+}
